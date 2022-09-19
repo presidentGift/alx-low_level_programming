2dbd43bb39cf2fcb60f7cd843b036939d255e96f
Gift Zikalala <giftzikalala67@gmail.com>
2022-09-19 12:23:16 -0700

    Question 4 retry

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line.
- * @str: input string.
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
  * Return: no return.
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int count = 0;
 
 	while (count >= 0)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
+		if (s[count] == '\0')
 			break;
-		}
-		_putchar(str[count]);
 		count++;
 	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
